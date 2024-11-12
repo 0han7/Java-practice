7db4a9f69bb99b5cb88f722a5c727ee452f9bf7c
Younghan Yonn <dudgks1172@naver.com>
2024-11-12 09:45:55 +0900

    Ex7_9 여러 종류의 객체를 배열로 다루기

--- src/Ex7_9.java
+++ src/Ex7_9.java
@@ -1,82 +1,82 @@
- class Product2 {
-        int price;
-        int bonusPoint;
+class Product2 {
+    int price;
+    int bonusPoint;
 
-        Product2(int price) {
-            this.price = price;
-            bonusPoint = (int) (price / 10.0);
-        }
-
-        Product2() {
-        }
+    Product2(int price) {
+        this.price = price;
+        bonusPoint = (int) (price / 10.0);
     }
 
-    class Tv2 extends Product2 {
-        Tv2() {
-            super(100);
-        }
+    Product2() {
+    }
+}
 
-        public String toString() {
-            return "Tv";
-        }
+class Tv2 extends Product2 {
+    Tv2() {
+        super(100);
     }
 
-    class Computer2 extends Product2 {
-        Computer2() {
-            super(200);
-        }
+    public String toString() {
+        return "Tv";
+    }
+}
 
-        public String toString() {
-            return "Computer";
-        }
+class Computer2 extends Product2 {
+    Computer2() {
+        super(200);
     }
 
-    class Audio2 extends Product2 {
-        Audio2() {
-            super(50);
-        }
+    public String toString() {
+        return "Computer";
+    }
+}
 
-        public String toString() {
-            return "Audio";
-        }
+class Audio2 extends Product2 {
+    Audio2() {
+        super(50);
     }
 
-    class Buyer2 {
-        int money = 1000;
-        int bonusPoint = 0;
-        Product2[] cart = new Product2[10];
-        int i = 0;
+    public String toString() {
+        return "Audio";
+    }
+}
 
-        void buy(Product2 product2) {
-            if (money < product2.price) {
-                System.out.println("잔액이 부족합니다.");
-                return;
-            }
-            money -= product2.price;
-            bonusPoint += product2.bonusPoint;
-            cart[i++] = product2;
-            System.out.println(product2 + " 을/를 구입하셨습니다.");
-        }
+class Buyer2 {
+    int money = 1000;
+    int bonusPoint = 0;
+    Product2[] cart = new Product2[10];
+    int i = 0;
 
-        void summary() {
-            int sum = 0;
-            String itemList = "";
-            for (int i = 0; i < cart.length; i++) {
-                if (cart[i] == null) break;
-                sum += cart[i].price;
-                itemList += cart[i] + ", "; // 문자열로 형변환
-            }
-            System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
-            System.out.println("구입하신 제품은 " + itemList + "입니다.");
+    void buy(Product2 product2) {
+        if (money < product2.price) {
+            System.out.println("잔액이 부족합니다.");
+            return;
         }
+        money -= product2.price;
+        bonusPoint += product2.bonusPoint;
+        cart[i++] = product2;
+        System.out.println(product2 + " 을/를 구입하셨습니다.");
     }
 
- public class Ex7_9 {
-     public static void main(String[] args) {
-         Buyer2 b = new Buyer2();
-         b.buy(new Tv2());
-         b.buy(new Computer2());
-         b.buy(new Audio2());
-         b.summary();
-     }
- }
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+        for (int i = 0; i < cart.length; i++) {
+            if (cart[i] == null) break;
+            sum += cart[i].price;
+            itemList += cart[i] + ", "; // 문자열로 형변환
+        }
+        System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
+        System.out.println("구입하신 제품은 " + itemList + "입니다.");
+    }
+}
+
+public class Ex7_9 {
+    public static void main(String[] args) {
+        Buyer2 b = new Buyer2();
+        b.buy(new Tv2());
+        b.buy(new Computer2());
+        b.buy(new Audio2());
+        b.summary();
+    }
+}
