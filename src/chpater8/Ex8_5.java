ec2f2309d8849306b87864f9aeae8b2d8a219aee
Younghan Yonn <dudgks1172@naver.com>
2024-11-13 11:39:05 +0900

    Chapter 8 printStackTrace()와 getMessage()

--- src/chpater8/Ex8_5.java
+++ src/chpater8/Ex8_5.java
@@ -5,8 +5,7 @@ public class Ex8_5 {
         System.out.println(1);
         System.out.println(2);
 
-
-        try {
+it         try {
             System.out.println(3);
             System.out.println(0 / 0); // 예외 발생
             System.out.println(4);
